0fd32f5849a15b3a41ef532188cbd88609a4b857
egoo00 <egooo914@gmail.com>
2025-05-30 09:39:28 +0300

    Update Result.java

--- src/main/java/com/example/distanceservice/dto/Result.java
+++ src/main/java/com/example/distanceservice/dto/Result.java
@@ -3,11 +3,6 @@ package com.example.distanceservice.dto;
 public class Result {
     private Geometry geometry;
 
-    public Geometry getGeometry() {
-        return geometry;
-    }
-
-    public void setGeometry(Geometry geometry) {
-        this.geometry = geometry;
-    }
+    public Geometry getGeometry() { return geometry; }
+    public void setGeometry(Geometry geometry) { this.geometry = geometry; }
 }
